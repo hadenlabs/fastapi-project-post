140cf061c2631d3b817ed63101f8f3fa183a3b78
Luis Mayta <luis@hadenlabs.com>
2023-05-21 18:53:15 -0300

    fix: build docker and docker-compose (#4)

--- tests/unit/test_settings.py
+++ tests/unit/test_settings.py
@@ -7,7 +7,7 @@ from mock import patch
 from app.core.config import Environment, get_env
 
 
-@patch.dict(os.environ, {"ENVIRONMENT": "testing"})
+@patch.dict(os.environ, {"STAGE": "testing"})
 def test_configuration_environment_test():
     settings: Environment = get_env()
     assert_that(settings.db_name.startswith("test"), True, settings.db_name)
